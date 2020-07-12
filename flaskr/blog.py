5cbeebf5abaea91e1e59bae9dc3fa195fd6accbc
Chen Hao Liu <chliu@gwu.edu>
2020-07-12 12:21:02 -0400

    media capture functionality

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -201,3 +201,11 @@ def delete(id):
     db.execute('DELETE FROM post WHERE id = ?', (id,))
     db.commit()
     return redirect(url_for('blog.index'))
+
+
+
+@bp.route('/<int:id>/captureImg', methods=('GET', 'POST'))
+@login_required
+def captureImg(id):
+    post = get_post(id)
+    return render_template('blog/imageCapture.html', post=post)
