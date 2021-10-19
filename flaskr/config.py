569615a7a9781492c01d097b8e12ddc64d7057a4
Chen Hao Liu <chhaoliu@gmail.com>
2021-10-19 01:18:51 +0000

    Updates to config

--- flaskr/config.py
+++ flaskr/config.py
@@ -1,19 +1,9 @@
 #Google Maps API KEY
-map_key = 'AIzaSyAo1kP1AprvXHOZPpBsZV75bl6wg7ByuXQ'
+map_key = 'KEY'
 #geoip API key
-geo_key = 'at_ZWBYLzwBQx7uClQFX6IOlqLPPbsDm'
+geo_key = 'KEY'
 #MySQL database
-username = 'chhaoliu'
-password = 'prepho2021chen!'
-database = 'chhaoliu$rephotoDB'
-host = 'chhaoliu.mysql.pythonanywhere-services.com'
-
-#Google Maps API KEY
-#map_key = 'AIzaSyAo1kP1AprvXHOZPpBsZV75bl6wg7ByuXQ'
-#geoip API key
-#geo_key = 'at_ZWBYLzwBQx7uClQFX6IOlqLPPbsDm'
-#MySQL database
-#username = 'chliu'
-#password = 'JDMKx49#2zp!'
-#database = 'chliu$rephotoBase'
-#host = 'chliu.mysql.pythonanywhere-services.com'
+username = 'USERNAME'
+password = 'USERNAME'
+database = 'DATABASE'
+host = 'HOST'
