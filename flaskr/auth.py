1f700445fac4c3aec6f26856066461f6ec43d3d8
ebrahimbonger <ebrahimbonger>
2022-02-10 00:42:25 +0000

    new pull made

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -93,7 +93,7 @@ def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
-            return redirect(url_for('auth.login'))
+            return redirect(url_for('auth.loginOrReg'))
 
         return view(**kwargs)
 
